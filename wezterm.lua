20727ed582308e9d290258b8748501e3bd7eff93
Kirill <67194208+kotikkir9@users.noreply.github.com>
2025-03-27 12:34:24 +0100

    Added window and tab title formatting.

--- wezterm.lua
+++ wezterm.lua
@@ -1,20 +1,57 @@
 local wez = require 'wezterm'
 local config = wez.config_builder()
 
-config.color_scheme = 'Kanagawa (Gogh)'
--- config.color_scheme = 'Kanagawa Dragon (Gogh)'
+-- OSC 7 on Windows with powershell
+-- https://wezterm.org/shell-integration.html#osc-7-on-windows-with-cmdexe
 
+if wez.target_triple == 'x86_64-pc-windows-msvc' then
+    config.default_prog = { "pwsh.exe", "-NoLogo" }
+end
+
+wez.on('format-tab-title', function(tab, _, _, _, _, _)
+    local title = tab.tab_title
+    if title == nil or #title == 0 then
+        local cwd = tab.active_pane.current_working_dir
+        if cwd then
+            local path = cwd.file_path:gsub("[/\\]$", "")
+            local item1, item2 = path:match("([^/\\]+)[/\\]([^/\\]+)$")
+            if item1 and item2 then
+                title = item1 .. "/" .. item2
+            else
+                title = path
+            end
+        end
+    end
+    return { { Text = title }, }
+end)
+
+wez.on('format-window-title', function(tab, _, tabs, _, _)
+    local index = ''
+    if #tabs > 1 then
+        index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+    end
+
+    local cwd = tab.active_pane.current_working_dir
+    if cwd then
+        local path = cwd.file_path:gsub("[/\\]$", "")
+        if path:sub(1, 3) == "/C:" then
+            path = path:sub(2)
+        end
+        return index .. " " .. path
+    end
+
+    return index .. "wezterm"
+end)
+
+config.color_scheme = 'Kanagawa (Gogh)'
 config.colors = {
     background = '#181818'
 }
 
-config.default_prog = { "pwsh" }
-
--- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-
--- config.font = w.font('JetBrainsMono Nerd Font', { weight = 'Regular' })
-config.font = wez.font('CaskaydiaCove Nerd Font', { weight = 'Regular' })
--- config.font_size = 10.5
+-- config.font = wez.font('JetBrainsMono Nerd Font', { weight = 'Regular' })
+-- config.font = wez.font('CaskaydiaCove Nerd Font', { weight = 'Regular' })
+config.font = wez.font('ComicShannsMono Nerd Font', { weight = 'Regular' })
+config.font_size = 11
 
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
@@ -32,7 +69,6 @@ config.keys = {
     { key = 'p', mods = 'SHIFT|ALT', action = wez.action.ActivateTabRelative(-1) },
     { key = 'n', mods = 'SHIFT|ALT', action = wez.action.ActivateTabRelative(1) },
     { key = 't', mods = 'SHIFT|ALT', action = wez.action.SpawnTab('CurrentPaneDomain') },
-
     {
         key = 'r',
         mods = 'SHIFT|ALT',
@@ -41,14 +77,13 @@ config.keys = {
             action = wez.action_callback(function(window, _, line)
                 -- line will be `nil` if they hit escape without entering anything
                 -- An empty string if they just hit enter
-                -- Or the actual line of text they wrote
+                -- Or thactual line of text they wrote
                 if line then
                     window:active_tab():set_title(line)
                 end
             end),
         }
     },
-
     { key = 'n', mods = 'CTRL|ALT', action = wez.action.MoveTabRelative(1) },
     { key = 'p', mods = 'CTRL|ALT', action = wez.action.MoveTabRelative(-1) },
 }
